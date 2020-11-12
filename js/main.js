0e148035d9f572a547f31a15487ea0fa4610f86f
atamvakis <tamvakis@live.unc.edu>
2020-11-11 21:20:58 -0500

    Updating

--- js/main.js
+++ js/main.js
@@ -2,111 +2,165 @@
 var markers = [
   {
     coords:{ lat: 36.035833, lng: -78.921183 },
-    content: '<h3>Duke Homestead</h3>'
+    title: 'Duke Homestead',
+    description:"See the early home, factories, and farm where Washington Duke first grew and processed tobacco. His sons later founded The American Tobacco Company, the world's largest tobacco company. Admission is free, but donations are welcome.",
+    img: 'img/pics/dukehomestea.jpg'
   },
   {
     coords:{ lat: 36.029213, lng: -78.975073 },
-    content: '<h3>Bennett Place</h3>'
+    title: 'Bennett Place',
+    description:"In April 1865, Confederate Gen. Joseph E. Johnston and Union Gen. William T. Sherman met at Bennett Place, where they signed surrender papers for Southern armies in the Carolinas, Georgia, and Florida. Admission is free, but donations are welcome.",
+    img: 'img/pics/bennett.jpg'
   },
   {
     coords:{ lat: 35.821592, lng: -80.897344 },
-    content: '<h3>Fort Dobbs</h3>'
+    title: 'Fort Dobbs',
+    description:"Ft. Dobbs provides a window into the tumultuous period of history known as the French and Indian War (1754-1763) or Seven Years War. As the only state historic site associated with the period, it represents North Carolina's link with a global war for empire that crossed five continents, lasted nearly a decade and sowed the seeds for independence. Free.",
+    img: 'img/pics/dobbs.jpg'
   },
   {
     coords:{ lat: 36.009738, lng: -79.520861 },
-    content: '<h3>Alamance Battleground</h3>'
+    title: 'Alamance Battleground',
+    description:"In 1771, an armed rebellion of backcountry farmers called Regulators battled with royal governor William Tryon's militia on land now preserved at Alamance Battleground State Historic Sites. Admission is free, but donations are welcome.",
+    img: 'img/pics/alamance.jpg'
   },
   {
     coords:{ lat: 34.236508, lng: -77.954311 },
-    content: '<h3>Battleship North Carolina</h3>'
+    title: 'Battleship North Carolina',
+    description:"One of the most decorated battleships of World War II that now serves as a memorial to all North Carolina veterans.",
+    img: 'img/pics/battleship.jpg'
   },
   {
     coords:{ lat: 35.302176, lng: -78.322470 },
-    content: '<h3>Bentonville Battlefield</h3>'
+    title: 'Bentonville Battlefield',
+    description:"The Battle of Bentonville, fought March 19-21, 1865, was the last full-scale action of the Civil War in which a Confederate army was able to mount a tactical offensive. This was the largest ever battle fought in North Carolina. Admission is free, but donations are welcome.",
+    img: 'img/pics/bentonville.jfif'
   },
   {
     coords:{ lat: 33.971752, lng: -77.917666 },
-    content: '<h3>Fort Fisher</h3>'
+    title: 'Fort Fisher',
+    description:"Until the last few months of the Civil War, Fort Fisher kept North Carolina's port of Wilmington open to blockade-runners supplying necessary goods to Confederate armies inland. When Ft. Fisher fell after a massive Federal amphibious assault on January 15, 1865, its defeat helped seal the fate of the Confederacy. Free Admission.",
+    img: 'img/pics/fisher.jpg'
   },
   {
     coords:{ lat: 36.119901, lng: -78.836680 },
-    content: '<h3>Historic Stagville</h3>'
+    title: 'Historic Stagville',
+    description:"Comprises the remains of North Carolina's largest pre-Civil War plantation and one of the South's largest. It once belonged to the Bennehan-Cameron family, whose combined holdings totaled approximately 900 slaves and almost 30,000 acres by 1860.",
+    img: 'img/pics/stagville.jpg'
   },
   {
     coords:{ lat: 35.285486, lng: -80.466463 },
-    content: '<h3>Reed Gold Mine</h3>'
+    title: 'Reed Gold Mine',
+    description:"Reed Gold Mine is the site of the first documented gold find in the United States. From this discovery, gold mining spread gradually to nearby counties and eventually into other southern states. Free admission, donations are accepted and appreciated.",
+    img: 'img/pics/reed.jpg'
   },
   {
     coords:{ lat: 35.789385, lng: -76.403968 },
-    content: '<h3>Somerset Place</h3>'
+    title: 'Somerset Place',
+    description:"Somerset Place is a representative state historic site offering a comprehensive and realistic view of 19th-century life on a large North Carolina plantation. Free admission, donations are accepted and appreciated.",
+    img: 'img/pics/somerset.jfif'
   },
   {
     coords:{ lat: 35.105765, lng: -77.044371 },
-    content: '<h3>Tryon Palace</h3>'
+    title: 'Tryon Palace',
+    description:"Historic homes and gardens crowned by a grand palace, all at North Carolina’s first state capitol.",
+    img: 'img/pics/tryon.jpg'
   },
   {
     coords:{ lat: 35.182620, lng: -79.929204 },
-    content: '<h3>Town Creek Indian Mound</h3>'
+    title: 'Town Creek Indian Mound',
+    description:"Town Creek Indian Mound is an unusual phenomenon in the history of North American archaeology. Situated on Little River (a tributary of the Great Pee Dee in central North Carolina), it has been the focus of a consistent program of archaeological research under one director for more than half a century. Free admission, donations are accepted and appreciated.",
+    img: 'img/pics/towncreek.jpg'
   },
   {
     coords:{ lat: 34.040498, lng: -77.945959 },
-    content: '<h3>Brunswick Town / Fort Anderson</h3>'
+    title: 'Brunswick Town / Fort Anderson',
+    description:"A major pre-Revolutionary port on North Carolina's Cape Fear River, Brunswick was razed by British troops in 1776 and never rebuilt. During the Civil War, Fort Anderson was constructed atop the old village site. Admission free, but donations welcome.",
+    img: 'img/pics/brunswick.jpg'
   },
   {
     coords:{ lat: 36.067864, lng: -79.624333 },
-    content: '<h3>Charlotte Hawkins Brown Museum</h3>'
+    title: 'Charlotte Hawkins Brown Museum',
+    description:"Founded in 1902 by Dr. Charlotte Hawkins Brown, Palmer Memorial Institute transformed the lives of more than 2,000 African American students. Today visitors can explore this unique environment where boys and girls lived and learned during the greater part of the 20th century. Free.",
+    img: 'img/pics/charlottehawkins.jfif'
   },
   {
     coords:{ lat: 35.261306, lng: -77.583213 },
-    content: '<h3>CSS Neuse & Gov. Caswell Memorial</h3>'
+    title: 'CSS Neuse & Gov. Caswell Memorial',
+    description:"Glimpses into two of our nation's most pivotal wars can be found in one historic site within the city of Kinston. Here you will explore the celebrated life of Richard Caswell, the first governor of the independent state of North Carolina.",
+    img: 'img/pics/cssneuse.jpg'
   },
   {
     coords:{ lat: 35.520168, lng: -77.976017 },
-    content: '<h3>Governor Charles B. Aycock Birthplace</h3>'
+    title: 'Governor Charles B. Aycock Birthplace',
+    description:"In 1900 he was elected governor and dedicated himself to improving public education in North Carolina. By the time he left office four years later, one school had been built for each day the governor was in office. Free Admission.",
+    img: 'img/pics/aycock.jpg'
   },
   {
     coords:{ lat: 35.466782, lng: -79.383874 },
-    content: '<h3>House in the Horseshoe</h3>'
+    title: 'House in the Horseshoe',
+    description:"In spring and summer, bright flowers surround this plantation house named for its location on a horseshoe bend in the Deep River. The house (ca. 1770) was owned by Philip Alston, whose band of colonists seeking independence from Britain was attacked here in 1781. Free, donations are accepted and appreciated.",
+    img: 'img/pics/horseshoe.jpg'
   },
   {
     coords:{ lat: 36.268108, lng: -80.484680 },
-    content: '<h3>Horne Creek Farm</h3>'
+    title: 'Horne Creek Farm',
+    description:"Once the Hauser family farm, Horne Creek enables visitors to experience farm life in North Carolina's northwestern Piedmont circa 1900. The site features the family's original farm house, a tobacco curing barn, a corn crib, adjacent fields under cultivation, and even a heritage apple orchard. Free, donations are accepted and appreciated.",
+    img: 'img/pics/hornecreek.jpg'
   },
   {
     coords:{ lat: 36.329965, lng: -77.589457 },
-    content: '<h3>Historic Halifax</h3>'
+    title: 'Historic Halifax',
+    description:"Located on the Roanoke River, the town of Halifax developed into a commercial and political center at the time of the American Revolution. A self-guided walking tour of the site includes several authentically restored and furnished buildings including the 1838 Jail, the 1790 Eagle Tavern and the Underground Railroad Trail.",
+    img: 'img/pics/halifax.jpg'
   },
   {
     coords:{ lat: 36.061395, lng: -76.607885 },
-    content: '<h3>Historic Edenton</h3>'
+    title: 'Historic Edenton',
+    description:"Featuring 18th and 19th century history, North Carolina's second oldest town Edenton was one of the fledgling nation's chief political, cultural, and commercial centers. The state's first colonial capital, it was established in the late 17th century and incorporated in 1722.",
+    img: 'img/pics/edenton.jpg'
   },
   {
     coords:{ lat: 35.476909, lng: -76.811692 },
-    content: '<h3>Historic Bath</h3>'
+    title: 'Historic Bath',
+    description:"European settlement near the Pamlico River in the 1690s led to the founding of Bath, North Carolina's first town, in 1705. By 1708, Bath had 50 people and 12 houses. Political rivalries, Indian wars, and piracy marked its early years but in 1746 Bath was considered for the colony's capital.",
+    img: 'img/pics/bath.jpg'
   },
   {
     coords:{ lat: 35.780306, lng: -78.639102 },
-    content: '<h3>N.C. State Capitol</h3>'
+    title: 'N.C. State Capitol',
+    description:"The North Carolina State Capitol, completed in 1840, is one of the finest and best-preserved examples of a major civic building in the Greek Revival style of architecture. It is a National Historic Landmark. Free admission, donations are accepted and appreciated.",
+    img: 'img/pics/capitol.jpg'
   },
   {
     coords:{ lat: 35.688025, lng: -80.431023 },
-    content: '<h3>North Carolina Transportation Museum</h3>'
+    title: 'North Carolina Transportation Museum',
+    description:"Discover the people and machines that have moved North Carolina, this is where locomotives that hauled Southern's passenger trains and freight trains filled with North Carolina furniture, textiles, tobacco, and produce were serviced from 1896 to the late 1970's.",
+    img: 'img/pics/transportation.jpg'
   },
   {
     coords:{ lat: 35.077812, lng: -80.881794 },
-    content: '<h3>President James K. Polk</h3>'
+    title: 'President James K. Polk',
+    description:"This site is located on land once owned by the parents of James K. Polk, the 11th U.S. president. The state historic site commemorates significant events in the Polk administration: the Mexican-American War, settlement of the Oregon boundary dispute, and the annexation of California. Free, donations are accepted and appreciated.",
+    img: 'img/pics/polk.gif'
   },
   {
     coords:{ lat: 35.597442, lng: -82.551315 },
-    content: '<h3>Thomas Wolfe Memorial</h3>'
+    title: 'Thomas Wolfe Memorial',
+    description:"Thomas Wolfe left an indelible mark on American letters. And his mother's boardinghouse in Asheville, North Carolina, now the Thomas Wolfe Memorial, has become one of literature's most famous landmarks.",
+    img: 'img/pics/wolfe.jpg'
   },
   {
     coords:{ lat: 35.910872, lng: -75.665750 },
-    content: '<h3>Roanoke Island Festival Park</h3>'
+    title: 'Roanoke Island Festival Park',
+    description:"Roanoke Island Festival Park is an interactive family attraction that celebrates the first English settlement in America. Visitors board and explore the park's centerpiece, a representational ship, Elizabeth II.",
+    img: 'img/pics/roanoake.png'
   },
   {
     coords:{ lat: 35.700903, lng: -82.496284 },
-    content: '<h3>Zebulon B. Vance Birthplace</h3>'
+    title: 'Zebulon B. Vance Birthplace',
+    description:'This pioneer farmstead, tucked in the beautiful Reems Creek Valley, features a two-story log cabin "mansion," an original 1790s slave cabin, and five outbuildings. Furnished as it would have been in the 1830s, the site explores life in early Buncombe County as seen through the lives of the Vance family and enslaved people. Free admission, donations are accepted and appreciated.',
+    img: 'img/pics/zebulon.jpg'
   }
 ];
 var listBuilder = "";
@@ -120,7 +174,7 @@ $(document).ready( function () {
 function initMap() {
   //Map Options, Map centered on NC
   var options = {
-    zoom: 8,
+    zoom: 7,
     center: { lat: 34.943999, lng: -79.242320}
   }
 
@@ -142,12 +196,13 @@ function initMap() {
 
     //Add the info windows and event listeners
     var infoWindow = new google.maps.InfoWindow({
-      content: props.content
+      content: '<h4>'+props.title+'</h4><br><img class="windowImg" src='+props.img+'><br><a href="#" class="dialogLink" onclick="$( \'#dialog\' ).dialog( \'open\' );">Learn More</a>'
     });
 
     marker.addListener('click', function(){
       infoWindow.open(map, marker);
     });
+
 
   }
 
@@ -160,10 +215,32 @@ function buildList(){
   listBuilder += "<ul>";
 
   for(var i = 0; i < markers.length; i++){
-    listBuilder += "<li class='listItem'>"+markers[i].content+"</li>"
+    listBuilder += "<li class='listItem'><h4>"+markers[i].title+"</h4>"
+      listBuilder += "<div class='row'>"
+        listBuilder += "<div class='col-md-4'>"
+          listBuilder+= "<img class='listImg' src="+markers[i].img+">"
+        listBuilder += "</div>"
+        listBuilder += "<div class='col-md-7'>"
+                listBuilder += "<p>"+markers[i].description+"</p>"
+        listBuilder += "</div>"
+      listBuilder += "</div>"
+    listBuilder += "</li>"
+    listBuilder += "<hr>"
   }
 
   listBuilder += "</ul>";
 
   list.innerHTML = listBuilder;
 }
+
+//Dialog Box
+$( function() {
+   $( "#dialog" ).dialog({
+     modal: true,
+     autoOpen: false
+   });
+
+   $( "#opener" ).on( "click", function() {
+     $( "#dialog" ).dialog( "open" );
+   });
+ } );
